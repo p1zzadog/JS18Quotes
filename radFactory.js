ae5ab8d038ed0b32cd9d5842aed13d4d94346aa6
Student <student@RU-103.local>
2015-09-23 12:38:36 -0600

    edit funcitonality working, data binding was wrong

--- radFactory.js
+++ radFactory.js
@@ -2,5 +2,21 @@ angular.module('radApp').factory('radFactory', [function(radFactory){
 
 	var quoteArray = [];
 
-	return quoteArray;
+	var Quote = function(quote, author) {
+		this.quote = quote;
+		this.author = author;
+		this.editValue = false;
+		this.quoteEdit = null;
+		this.authorEdit = null;
+		quoteArray.push(this);
+	};
+
+	new Quote("The sky was the color of television, tuned to a dead channel.", 'William Gibson');
+	new Quote("I choose my friends for their good looks, my acquaintances for their good characters, and my enemies for their good intellects.", 'Oscar Wilde');
+
+	return {
+		Quote      : Quote,
+		quoteArray : quoteArray
+	};
+
 }]);
--- radTroller.js
+++ radTroller.js
@@ -1,9 +1,8 @@
-angular.module('radApp', []);
+angular.module('radApp', ['ngAnimate']);
 
 angular.module('radApp').controller('radTroller',  ['$scope', '$timeout', 'radFactory', function($scope, $timeout, radFactory){
 
-	$scope.quoteArray = radFactory;
-
+	$scope.quoteArray = radFactory.quoteArray;
 	$scope.formToggle = false;
 	$scope.thanksMessage = false;
 	$scope.formButtonString = "Add a Quote!"
@@ -26,12 +25,8 @@ angular.module('radApp').controller('radTroller',  ['$scope', '$timeout', 'radFa
 	};
 
 	$scope.submitForm = function() {
-		var quoteEntry = {
-			quote  : $scope.newQuote,
-			author : $scope.newAuthor
-			// Rating: 
-		}
-		$scope.quoteArray.push(quoteEntry)
+		new radFactory.Quote($scope.newQuote, $scope.newAuthor);
+			console.log($scope.newQuote, $scope.newAuthor);
 		$scope.newQuote = null;
 		$scope.newAuthor = null;
 		$scope.formToggle = !$scope.formToggle;
@@ -47,19 +42,30 @@ angular.module('radApp').controller('radTroller',  ['$scope', '$timeout', 'radFa
 // Existing Quote Edit
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+	var tempQuoteStore  = null;
+	var tempAuthorStore = null;
+
 	$scope.editQuote = function(index) {
 		$scope.quoteArray[index].editValue = true;
+		tempQuoteStore  = $scope.quoteArray[index].quote;
+		tempAuthorStore = $scope.quoteArray[index].author;
 	};
 
-	$scope.submitEdit = function(index) {
-		$scope.quoteArray[index].quote = $scope.quoteEdit;
-		$scope.quoteArray[index].author = $scope.authorEdit;
+	$scope.submitEdit = function(index, event) {
+		event.stopPropagation();
+		// $scope.quoteArray[index].quote = $scope.quoteArray[index].quoteEdit;
+		// $scope.quoteArray[index].author = $scope.quoteArray[index].authorEdit;
+		// 	console.log($scope.quoteArray[index].quoteEdit, $scope.quoteArray[index].authorEdit);
+		// $scope.quoteArray[index].quoteEdit = null;
+		// $scope.quoteArray[index].authorEdit	=null;
 		$scope.quoteArray[index].editValue = false;
-		console.log($scope.quoteArray[index])
 	};
 
-	$scope.cancelEdit = function(index) {
+	$scope.cancelEdit = function(index, event) {
+		event.stopPropagation();
 		$scope.quoteArray[index].editValue = false;
+		$scope.quoteArray[index].quote  = tempQuoteStore;
+		$scope.quoteArray[index].author = tempAuthorStore;
 	};
 
 }]);
